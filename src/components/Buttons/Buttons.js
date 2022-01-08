bef32b03c047e4eed98f4dda848a84c37feb3748
Mmackz <matthewmackenz@gmail.com>
2022-01-07 20:58:53 -0800

    ➕ import useRef hook

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -10,38 +10,45 @@ const options = {
 function Buttons(props) {
    const { state, dispatch } = props;
 
-   const ref = useRef(state)
+   const ref = useRef(state);
 
    function evaluateExpression() {
-      console.log("evaluating")
+      console.log("evaluating");
       console.log(ref.current.display);
       const ans = format(evaluate(ref.current.display), options);
       console.log(ans);
    }
 
-   useEffect(() => {
-      ref.current = state
-   }, [state])
-
-   useEffect(() => {
-      function handleKeypress(e) {
-         if (buttons.flat().includes(e.key)) {
-            if (e.key === "=") {
-               console.log("calculating...");
-               evaluateExpression()
-            }
-            dispatch({ type: "display", payload: e.key });
+   function handleButtonPress(key) {
+      console.log(key);
+      if (buttons.flat().includes(key)) {
+         if (key === "=" || key === "Enter") {
+            console.log("calculating...");
+            evaluateExpression();
          }
+         dispatch({ type: "display", payload: key });
       }
+   }
 
-      window.addEventListener("keypress", handleKeypress);
-      return () => window.removeEventListener("keypress", handleKeypress);
+   useEffect(() => {
+      ref.current = state;
+   }, [state]);
+
+   useEffect(() => {
+      window.addEventListener("keydown", (e) => handleButtonPress(e.key));
+      return () =>
+         window.removeEventListener("keydown", (e) => handleButtonPress(e.key));
+   // eslint-disable-next-line react-hooks/exhaustive-deps
    }, []);
 
    return (
       <section className="button-container">
          {buttons.flat().map((symbol) => (
-            <div key={symbol} className="button">
+            <div
+               key={symbol}
+               className="button"
+               onClick={() => handleButtonPress(symbol)}
+            >
                {symbol}
             </div>
          ))}
