399693d6d8a5e12dd46a28be39a4144b62eca3f0
Mmackz <matthewmackenz@gmail.com>
2022-01-09 13:38:05 -0800

    ➖ remove unused code

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -1,31 +1,20 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import buttons from "../../helpers/buttons";
 
 function Buttons(props) {
-   const { state, dispatch } = props;
-
-   // const ref = useRef(state);
+   const { dispatch } = props;
 
    function handleButtonPress(key) {
-      console.log(key);
       if (buttons.flat().includes(key) || key === "Enter") {
-         // if (key === "=" || key === "Enter") {
-         //    console.log("calculating...");
-         //    evaluateExpression();
-         // }
-         dispatch({ type: "display", payload: key });
+         dispatch({ payload: key });
       }
    }
-
-   // useEffect(() => {
-   //    ref.current = state;
-   // }, [state]);
 
    useEffect(() => {
       window.addEventListener("keydown", (e) => handleButtonPress(e.key));
       return () =>
          window.removeEventListener("keydown", (e) => handleButtonPress(e.key));
-   // eslint-disable-next-line react-hooks/exhaustive-deps
+      // eslint-disable-next-line react-hooks/exhaustive-deps
    }, []);
 
    return (
